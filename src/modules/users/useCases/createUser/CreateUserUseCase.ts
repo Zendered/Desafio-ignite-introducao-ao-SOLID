cd59d8b6e1aafac4b187f77cfd3ff193fbdfe49a
zend <luizz.ggr@gmail.com>
2021-11-03 22:14:41 -0300

    Desafio 01 - Introdução ao SOLID

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,17 +1,25 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userExists = this.usersRepository.findByEmail(email);
+
+        if (userExists) {
+            throw new Error('User already exists')
+        }
+
+        const user = this.usersRepository.create({ name, email });
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
